247494616cb44e74099182de89f0bf2056b57e6d
ReneHalle <renehalle802@gmail.com>
2021-08-17 12:33:36 +0200

    Exercice 4

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(' #button_green').click(function(){
+        $('#p1').css('color','green')
+    });
+    $(' #button_red').click(function(){
+        $('#p1').css('color','red')
+    });
+});
